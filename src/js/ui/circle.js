05c2e3bfd871b033980546850962549b833e4878
Iñaki Abadia <i.abadiaosta@gmail.com>
2016-03-09 00:11:06 +0100

    Project structure updated, now compatible with SDK instead of CloudPebble

---
+++ src/js/ui/circle.js
@@ -0,0 +1,18 @@
+var util2 = require('util2');
+var myutil = require('myutil');
+var StageElement = require('ui/element');
+
+var defaults = {
+  backgroundColor: 'white',
+  borderColor: 'clear',
+  borderWidth: 1,
+};
+
+var Circle = function(elementDef) {
+  StageElement.call(this, myutil.shadow(defaults, elementDef || {}));
+  this.state.type = StageElement.CircleType;
+};
+
+util2.inherit(Circle, StageElement);
+
+module.exports = Circle;
